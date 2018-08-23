e1e964f463b804e8e17818a9080aed75b3f86e85
jantzeno <jantzeno@msn.com>
2018-08-23 13:13:46 -0400

    nim: moved header and pixels to separate generator functions

--- 1_OneWeekend/1_Graphics_Hello_World/nim/ppm.nim
+++ 1_OneWeekend/1_Graphics_Hello_World/nim/ppm.nim
@@ -1,31 +1,43 @@
 
 proc outputFile(fileName: string, header: string, pixels: seq[string]): bool =
-    var output = open(fileName, fmWrite)
-    try:
-        output.writeLine(header)
-        for p in pixels:
-            output.writeLine(p)
-        return true
-    except:
-        echo "Any error occured writing image"
-    return false
+  var output = open(fileName, fmWrite)
+  try:
+    output.writeLine(header)
+    for p in pixels:
+      output.writeLine(p)
+    return true
+  except:
+    echo "Any error occured writing image"
+  return false
 
+proc generateHeader(x: int, y: int): string =
+  var header: string = "P3\n" & $x & " " & $y & "\n255"
+  return header
 
-let nx = 200
-let ny = 100
-var pixels = newSeq[string]()
-var header: string = "P3\n" & $nx & " " & $ny & "\n255"
-var rgb: string
-for j in countdown(ny-1, 0):
+proc generatePixels(x: int, y: int): seq[string] =
+  let nx = x
+  let ny = y
+  var pixels = newSeq[string]()
+  var rgb: string
+
+  for j in countdown(ny-1, 0):
     for i in 0..<nx:
-        let r: float = toFloat(i) / toFloat(nx)
-        let g: float = toFloat(j) / toFloat(ny)
-        let b: float = 0.2
-        let ir: int = toInt(255.99*r)
-        let ig: int = toInt(255.99*g)
-        let ib: int = toInt(255.99*b)
-        rgb = $ir & " " & $ig & " " & $ib
-        pixels.add(rgb)
+      let r: float = toFloat(i) / toFloat(nx)
+      let g: float = toFloat(j) / toFloat(ny)
+      let b: float = 0.2
+      let ir: int = toInt(255.99*r)
+      let ig: int = toInt(255.99*g)
+      let ib: int = toInt(255.99*b)
+      rgb = $ir & " " & $ig & " " & $ib
+      pixels.add(rgb)
 
-var image: bool
-image = outputFile("nimimage.ppm", header, pixels)
+  return pixels
+
+# main
+let x = 200
+let y = 100
+let header = generateHeader(x, y)
+let pixels = generatePixels(x, y)
+let image = outputFile("nimimage.ppm", header, pixels)
+if image == false:
+  echo "Error writing image file"
