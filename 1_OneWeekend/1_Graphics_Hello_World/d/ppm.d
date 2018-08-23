af1128870ca19358928a4e7535a8e8fdfc278dcf
jantzeno <jantzeno@msn.com>
2018-08-23 12:56:27 -0400

    d: moved header and pixels to separate generator functions

--- 1_OneWeekend/1_Graphics_Hello_World/d/ppm.d
+++ 1_OneWeekend/1_Graphics_Hello_World/d/ppm.d
@@ -3,35 +3,47 @@ import std.stdio;
 
 bool outputFile (string fileName, string header, string[] pixels) {
     try {
-        auto output = File(fileName, "w");
-        output.write(header);
-        foreach (line; pixels) {
-            output.writeln(line);
-        }
-        return true;
+      auto output = File(fileName, "w");
+      output.write(header);
+      foreach (line; pixels) {
+        output.writeln(line);
+      }
+      return true;
     } catch (Exception exc) {
         writeln("Any error occured writing image");
     }
     return false;
 }
 
-void main() {
-    int nx = 200;
-    int ny = 100;
-    string[] pixels;
-    string header = "P3\n" ~ to!string(nx) ~ " " ~ to!string(ny) ~ "\n255\n";
-    string rgb;
-    for (int j = ny-1; j >= 0; j--) {
-        foreach (i; 0..nx) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = to!int(255.99*r);
-            int ig = to!int(255.99*g);
-            int ib = to!int(255.99*b);
-            rgb = to!string(ir) ~ " " ~ to!string(ig) ~ " " ~ to!string(ib);
-            pixels ~= rgb;
-        }
+string generateHeader(int x, int y) {
+  string header = "P3\n" ~ to!string(x) ~ " " ~ to!string(y) ~ "\n255\n";
+  return header;
+}
+
+string[] generatePixels(int x, int y) {
+  int nx = x;
+  int ny = y;
+  string[] pixels;
+  string rgb;
+  for (int j = ny-1; j >= 0; j--) {
+    foreach (i; 0..nx) {
+      float r = float(i) / float(nx);
+      float g = float(j) / float(ny);
+      float b = 0.2;
+      int ir = to!int(255.99*r);
+      int ig = to!int(255.99*g);
+      int ib = to!int(255.99*b);
+      rgb = to!string(ir) ~ " " ~ to!string(ig) ~ " " ~ to!string(ib);
+      pixels ~= rgb;
     }
-    outputFile("dimage.ppm", header, pixels);
+  }
+  return pixels;
+}
+
+void main() {
+  int x = 200;
+  int y = 100;
+  string header = generateHeader(x, y);
+  string[] pixels = generatePixels(x, y);
+  outputFile("dimage.ppm", header, pixels);
 }
